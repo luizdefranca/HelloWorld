1c165e564c1790134dd074821512588ae1961405
Luiz Carlos de França Ramos <luizramospe@hotmail.com>
2019-03-13 19:15:43 -0400

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Luiz on 3/13/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
